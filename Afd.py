3e32b312d9d555d1f5d682e9a447a7f01aaf4dd9
Joao-Lamounier <jjoaolamounier@gmail.com>
2023-09-16 17:14:27 -0300

    Salvar o AFD de um arquivo

--- Afd.py
+++ Afd.py
@@ -1,18 +1,33 @@
+import copy
+
+
 class Afd:
 
     def __init__(self):
         self.states = set()
         self.alphabet = ""
-        self.transctions = dict()
-        self.inital = None
+        self.transitions = dict()
+        self.initial = None
         self.finals = set()
+        self.__curr_state = None
+        self.__error = False
+
+    def error(self):
+        return self.__error
+
+    def curr_state(self):
+        return self.__curr_state
+
+    def checked_transitions(self, start_id, symbol):
+        return (start_id, symbol) in self.transitions
 
     def create_state(self, key, initial=False, final=False):
         key = int(key)
         self.states.add(key)
 
         if initial:
-            self.inital = key
+            self.initial = key
+            self.__curr_state = self.initial
         elif final:
             self.finals.add(key)
 
@@ -22,19 +37,20 @@ class Afd:
         destin = int(destin)
         if symbol not in self.alphabet or origin not in self.states or destin not in self.states:
             return False
-        self.transctions[origin, symbol] = destin
+        self.transitions[origin, symbol] = destin
         return True
+
+    def is_final(self, id) -> bool:
+        return id in self.finals
+
     def load_afd(self, content):
-        # Salvando o os estados
         state_values = map(int, content[0].split())
 
         for value in state_values:
             self.create_state(value)
 
-        # Salvando o alfabeto
         self.alphabet = ''.join(map(str, content[1]))
 
-        # Salvando as funções de transição
         aux = self.alphabet.replace(' ', '')
         n_functions = len(self.states) * (len(aux) - 1)
 
@@ -43,11 +59,32 @@ class Afd:
             origin, symbol, destin = map(int, values)
             self.create_transition(origin, symbol, destin)
 
-        #Salvando o estado inicial
-        self.create_state(content[n_functions+2], True)
+        self.create_state(content[n_functions + 2], True)
 
-        #Salvando o(s) estado(s) fina(is)l
-        state_values = map(int, content[n_functions+3].split())
+        state_values = map(int, content[n_functions + 3].split())
 
         for value in state_values:
             self.create_state(value, False, True)
+
+    def convert_format_file(self):
+        content = ' '.join(map(str, self.states)) + '\n'
+        content += self.alphabet
+
+        for key, value in self.transitions.items():
+            transitions = f"{key[0]} {key[1]} {value}\n"
+            content += transitions
+
+        content += str(self.initial) + '\n'
+        content += ' '.join(map(str, self.finals)) + '\n'
+        return content
+
+    def start(self, word):
+        for symbol in word:
+            if symbol not in self.alphabet and not self.checked_transitions(self.__curr_state, symbol):
+                self.__error = True
+                break
+            self.__curr_state = self.transitions[(self.__curr_state, symbol)]
+        return self.__curr_state
+
+    def copy(self):
+        return copy.deepcopy(self)
--- main.py
+++ main.py
@@ -10,15 +10,29 @@ def read_file(file_name: str):
         raise FileNotFoundError(f'O arquivo "{file_name}" não foi encontrado.')
 
 
+def write_file(file_name: str, content: str):
+    try:
+        with open(file_name, 'w') as file:
+            file.write(content)
+    except Exception:
+        raise print(f'Não foi possível escrever em "{file_name}"')
 
 if __name__ == '__main__':
     automato = Afd()
     content = read_file('exemple.txt')
 
     automato.load_afd(content)
-    print(automato.states)
-    print(automato.alphabet)
-    print(automato.transctions)
-    print(automato.inital)
-    print(automato.finals)
+    write_file('exemple1.txt', automato.convert_format_file())
+    cadeia = '1'
+    state = automato.start(cadeia)
+
+    if automato.is_final(state) and not automato.error():
+        print('aceitou')
+    else:
+        print('rejeitou')
+
+    copy = automato.copy()
+    write_file('exemple3.txt', copy.convert_format_file())
+
+
 
